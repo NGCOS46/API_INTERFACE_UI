50faaf4201e32f36bccfcb8e344facc7fb198aed
ngcos <nickgcd@gmail.com>
2024-02-05 19:02:08 +0100

    Repository creacion

--- src/main/java/API/repository/ProveedorRepository.java
+++ src/main/java/API/repository/ProveedorRepository.java
@@ -1,8 +1,10 @@
 package API.repository;
-import API.model.productos;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import API.model.proveedor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductosRepository  extends JpaRepository<productos,Integer> {
+public interface ProveedorRepository extends JpaRepository<proveedor,Integer> {
+
 }
