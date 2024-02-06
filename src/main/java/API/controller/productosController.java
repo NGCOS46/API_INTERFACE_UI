95541cf839b890f74642c00c4c53b608055011a8
ngcos <nickgcd@gmail.com>
2024-02-06 11:03:04 +0100

    controllers creados

--- src/main/java/API/controller/productosController.java
+++ src/main/java/API/controller/productosController.java
@@ -1,4 +1,34 @@
 package API.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import API.dto.productosDTO;
+import API.model.productos;
+import API.service.productoService;
 
+import java.net.http.HttpResponse;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/ui/productos")
 public class productosController {
+    @Autowired
+    private productoService productoService;
+
+    @PostMapping
+    public ResponseEntity<productosDTO> save(@RequestBody productosDTO productosDTO){
+        return new ResponseEntity<>(productoService.save(productosDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<productosDTO>> findAll(){
+        return new ResponseEntity<>(productoService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{idproducto}")
+    public ResponseEntity<productosDTO> findById(@PathVariable int idproducto){
+        return new ResponseEntity<>(productoService.findById(idproducto), HttpStatus.OK);
+
+    }
 }
--- src/main/java/API/controller/proveedorController.java
+++ src/main/java/API/controller/proveedorController.java
@@ -1,4 +1,48 @@
 package API.controller;
-
+import API.dto.proveedorDTO;
+import API.service.proveedorService;
+import API.model.proveedor;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+@RestController
+@RequestMapping("api/ui/productos")
 public class proveedorController {
+
+    @Autowired
+    private proveedorService proveedorService;
+
+    @PostMapping
+    public ResponseEntity<proveedorDTO> save(@RequestBody proveedorDTO proveedorDTO) {
+        proveedorDTO savedProveedorDTO = proveedorService.save(proveedorDTO);
+        return new ResponseEntity<>(savedProveedorDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<proveedorDTO>> findAll() {
+        List<proveedorDTO> proveedoresDTO = proveedorService.findAll();
+        return new ResponseEntity<>(proveedoresDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("/{idProveedor}")
+    public ResponseEntity<proveedorDTO> findById(@PathVariable int idProveedor) {
+        proveedorDTO proveedorDTO = proveedorService.findById(idProveedor);
+        if (proveedorDTO != null) {
+            return new ResponseEntity<>(proveedorDTO, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{idProveedor}")
+    public ResponseEntity<Void> deleteById(@PathVariable int idProveedor) {
+        try {
+            proveedorService.deleteById(idProveedor);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
